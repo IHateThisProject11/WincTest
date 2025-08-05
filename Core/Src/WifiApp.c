0905919d3355fb38caf0c70f54a41821fbb6934f
IHateThisProject11 <misterbozo11@gmail.com>
2025-08-05 10:24:03 -0700

    Pre Generate Code

--- Core/Src/WifiApp.c
+++ Core/Src/WifiApp.c
@@ -22,7 +22,7 @@ void wifi_cb(uint8_t u8MsgType, void *pvMsg)
     {
         tstrM2mWifiStateChanged *pstrWifiState = (tstrM2mWifiStateChanged *)pvMsg;
         if (pstrWifiState->u8CurrState == M2M_WIFI_CONNECTED) {
-            // Station connected
+            M2M_PRINT("==> Connected to AP, requesting DHCP…\r\n");
         } else if (pstrWifiState->u8CurrState == M2M_WIFI_DISCONNECTED) {
             printf("Station disconnected\r\n");
         }
@@ -39,7 +39,7 @@ void wifi_cb(uint8_t u8MsgType, void *pvMsg)
     case M2M_WIFI_REQ_DHCP_CONF:
     {
         uint8_t *ip = (uint8_t*)pvMsg;
-        printf("DHCP - IP address is %u.%u.%u.%u\r\n",
+        M2M_PRINT("==> DHCP IP acquired: %u.%u.%u.%u\r\n",
                 ip[0], ip[1], ip[2], ip[3]);
         break;
     }
@@ -115,6 +115,8 @@ void WifiApp_InitAP(void)
     } else {
         printf("Connecting to %s …\r\n", MAIN_WLAN_SSID);
     }
+    printf("m2m_wifi_connect() returned %d\r\n", ret);
+
 }
 
 /**
